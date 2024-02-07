2afbda9231f26a9e464bcc84cd642364e48ce315
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2024-02-08 00:45:04 +0530

    github apis integreated

--- RepoExplorer/src/App.tsx
+++ RepoExplorer/src/App.tsx
@@ -1,15 +1,18 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./page/Home"
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./page/Home";
+import { UserContext } from "./context/UserContext";
+import { useState } from "react";
 
 function App() {
+  const [userData, setUserData] = useState({});
+
   return (
-    <>
+    <UserContext.Provider value={{ userData, setUserData }}>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </>
-  )
+    </UserContext.Provider>
+  );
 }
 
-export default App
+export default App;
--- RepoExplorer/src/components/SearchBar.tsx
+++ RepoExplorer/src/components/SearchBar.tsx
@@ -1,10 +1,34 @@
+import axios from "axios";
+import { UserContext } from "../context/UserContext";
+import { useContext, useEffect, useState } from "react";
+
 const SearchBar = () => {
+  const [searchInput, setSearchInput] = useState("pranitpatil03");
+
+  const { userData, setUserData } = useContext(UserContext);
+
+  const fetchData = async () => {
+    const data = await axios.get(`https://api.github.com/users/${searchInput}`);
+    setUserData(data.data);
+  };
+
+  const handleChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  useEffect(() => {
+    fetchData();
+    console.log(userData);
+  }, [searchInput]);
+
   return (
     <>
       <div className="flex items-center justify-center w-full relative">
         <input
+          value={searchInput}
           type="text"
           name="search"
+          onChange={handleChange}
           placeholder="Search by github username"
           className="my-6 py-3 px-12 outline-none rounded-xl border border-gray-300	w-11/12"
         />
