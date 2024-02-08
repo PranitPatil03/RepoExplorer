edbe183a73da1e5e7393241820fc13c891a45fc9
Pranit <pranit.201533105@vcet.edu.in>
2024-02-08 09:57:52 +0530

    feature completed

--- RepoExplorer/src/App.tsx
+++ RepoExplorer/src/App.tsx
@@ -1,14 +1,17 @@
+import { useEffect, useState } from 'react';
 import { Route, Routes } from "react-router-dom"
 import Home from "./page/Home"
-
+import { UserContext } from './context/UserContext';
 
 function App() {
+  const [userData, setUserData] = useState({});
+
   return (
-    <>
+    <UserContext.Provider value={{ userData, setUserData }}>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </>
+    </UserContext.Provider>
   )
 }
 
